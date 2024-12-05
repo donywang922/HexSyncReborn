7017bf32d0c41515bb25a0767afe7bf7ce738948
YWsuoyi <donywang922@gmail.com>
2024-12-05 03:31:23 -0800

    init

---
+++ src/main/java/com/ywsuoyi/hexsyncreborn/HexSyncRebornApplication.java
@@ -0,0 +1,13 @@
+package com.ywsuoyi.hexsyncreborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexSyncRebornApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexSyncRebornApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ywsuoyi/hexsyncreborn/HexSyncRebornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ywsuoyi.hexsyncreborn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexSyncRebornApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
